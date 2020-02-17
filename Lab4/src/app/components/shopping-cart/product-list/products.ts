30e062d10d31b458ca6fd307673be0568a481391
tdinaraa <45028995+tdinaraa@users.noreply.github.com>
2020-02-17 06:04:34 +0600

    labs yas!

---
+++ Lab4/src/app/components/shopping-cart/product-list/products.ts
@@ -0,0 +1,84 @@
+export const items = [
+  {
+    name: 'FHLDING`s Bag',
+    description: 'Сумки из натуральной кожи,Сумка-мессенджерСумка, кошелек, карта. Стиль: модный тренд, колледж. Размер: Большой: 26*25*8 (В * Д * Ш)/10,2*9,8*3,1,Середина: 15*18*6/5,9*7,1*2,4, маленький: 11*20/4,3*2,04, посылка с картой: 11,5*8/4,5*3,14. Материал:Из искусственной кожи.',
+    link: 'https://aliexpress.ru/item/4000378906973.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-6&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/H9153081cdb744f21bb5f681a553b67e8C/4.jpg_640x640.jpg',
+    rating: '5.0',
+    price: '416,78.'
+  },
+  {
+    name: 'DKQWAIT`s Bag',
+    description: 'Новая женская сумка, женская сумка, соломенная сумка, большие сумки для женщин, 2019, новые цветные сумочки для плетения, модные сексуальные повседневные сумки',
+    link: 'https://aliexpress.ru/item/32982550834.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-12&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/Hf3d308cdf4784d1795066c2c36d631a0s/-.jpg',
+    rating: '4.8',
+    price: '1 042,60 руб.'
+  },
+  {
+    name: 'Micky Ken`s Bag',
+    description: 'Женский комплект сумок из 2 предметов, кожаная сумка, женская сумка-тоут, женская сумка на плечо, 2019.',
+    link: 'https://aliexpress.ru/item/32995255898.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-15&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/HTB1kZNiNwHqK1RjSZFgq6y7JXXaI/2.jpg',
+    rating: '4.6',
+    price: '881,25 - 1 586,76 руб.'
+  },
+  {
+    name: 'SWDF`s Bag',
+    description: 'SWDF новые высококачественные женские сумки, дизайнерские сумки от известного бренда, женские сумки, женские сумки на плечо, сумки-мессенджеры с клапаном. Внутренняя часть: Внутренний карманный отсек.',
+    link: 'https://aliexpress.ru/item/33049399194.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-20&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/HTB1pfH1d.uF3KVjSZK9q6zVtXXa9/SWDF.jpg',
+    rating: '4.7',
+    price: '763,01 руб.'
+  },
+  {
+    name: 'Zer`s Bag',
+    description: 'Большая вместительная Женская сумка через плечо высокого качества 2020 мягкие холщовые женские кошельки и ручные сумки # Zer',
+    link: 'https://aliexpress.ru/item/32945391498.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-30&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/HTB1_woPXinrK1RjSsziq6xptpXay/-.jpg',
+    rating: '4.7',
+    price: '267,19 - 599,04 руб.'
+  },
+  {
+    name: 'Honey & Cute`s Bag',
+    description: 'Качественные с каменным узором кожаные сумки через плечо для женщин дизайнерские маленькие сумки на цепочке сумка через плечо мини-кошельки Ручная сумка',
+    link: 'https://aliexpress.ru/item/4000096106313.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-29&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/Hd79831def78243a1b5532fe10df6306fx/-.jpg',
+    rating: '4.7',
+    price: '881,90 руб.'
+  },
+  {
+    name: 'Louie vuiton`s Bag',
+    description: 'Сумки Роскошные сумки женские сумки дизайнерские модные сумки на плечо женская сумка через плечо Маленькая клетчатая сумка через плечо для женщин',
+    link: 'https://aliexpress.ru/item/4000525269820.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-41&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/Hc4189c6a0fd846a387dfd60024b0a1adL/-.jpg',
+    rating: '5.0',
+    price: '4 311,49 руб.'
+  },
+  {
+    name: 'ZERO MORE`s Bag',
+    description: '2020 роскошные женские сумки известного бренда, дизайнерские женские классические клетчатые сумки через плечо, кожаные женские сумки-мессенджеры',
+    link: 'https://aliexpress.ru/item/4000133973598.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-44&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/H09879662e7124cc5b5505ced170157e2f/2020.jpg',
+    rating: '4.6',
+    price: '1 043,91 - 1 172,60 руб.'
+  },
+  {
+    name: 'Litthing`s Bag',
+    description: 'Фрукты, авокадо, маленькая сумочка в коробке, сумка через плечо, сумка через плечо, сумка-арбуз, модная сумка-мессенджер',
+    link: 'https://aliexpress.ru/item/4000145202524.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-51&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/Hf4fd2011475a4740813e30ed02ec3f9ej/-.jpg',
+    rating: '4.0',
+    price: '254,77 - 573,56 руб.'
+  },
+  {
+    name: 'MANFUNI`s Bag',
+    description: 'Леопардовые сумки-шопперы для женщин 2019 роскошные сумки женские дизайнерские сумки через плечо с ручкой женские сумки через плечо Горячая Распродажа',
+    link: 'https://aliexpress.ru/item/32960551793.html?spm=a2g0o.productlist.0.0.43f02746fX2lDj&algo_pvid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b&algo_expid=94a672d0-7ae4-49ce-8ff9-e8b28525fe1b-55&btsid=0ab50f6115818801626081006e018c&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+    image: 'https://ae01.alicdn.com/kf/H0ec5c47792fa40eeb07f364cc686af7a7/2019.jpg',
+    rating: '4.7',
+    price: '712,05 руб.'
+  },
+
+
+];
